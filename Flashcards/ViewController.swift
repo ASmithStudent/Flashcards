aaa40dea977b73c1ab24d14ee695717160c0369a
ASmithStudent <adrian.smith@k12.wcsdny.org>
2018-10-13 13:36:04 -0700

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Adrian Smith on 10/13/18.
+//  Copyright © 2018 Adrian Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
